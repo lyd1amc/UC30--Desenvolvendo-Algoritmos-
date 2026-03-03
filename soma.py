4429136f0fc60767992cf13b179e7eb524985cb0
lyd1amc <lydiafernandinha@gmail.com>
2026-03-03 08:39:44 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,17 @@
+cont = 1
+while cont <= 10:
+    print("Olá, mundo!") 
+    cont += 1 # cont = cont + 1
+print("FIM") 
+
+print(list(range(5)))
+print(list(range(1,10,2))) 
+print(list(range(1,10)))
+
+soma = 0
+for termo in range(1,11):
+    soma += termo 
+print(soma)
+for cont in range(10):
+    print("Olá, mundo!")
+print("FIM") 
